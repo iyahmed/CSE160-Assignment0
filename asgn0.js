deeb7b4ae6e25b383cee3fd6b3a57260c845bce0
Ismail Ahmed <30091934+iyahmed@users.noreply.github.com>
2024-04-02 15:14:40 -0700

    Second Commit: Trying to publish to GitHub Pages.

--- asgn0.js
+++ asgn0.js
@@ -1,11 +1,11 @@
 // DrawTriangle.js (c) 2012 matsuda
-function main() {  
+function main() {
   // Retrieve <canvas> element
-  var canvas = document.getElementById('example');  
-  if (!canvas) { 
+  var canvas = document.getElementById('example');
+  if (!canvas) {
     console.log('Failed to retrieve the <canvas> element');
-    return false; 
-  } 
+    return false;
+  }
 
   // Get the rendering context for 2DCG
   var ctx = canvas.getContext('2d');
@@ -14,3 +14,19 @@ function main() {
   ctx.fillStyle = 'rgba(0, 0, 255, 1.0)'; // Set color to blue
   ctx.fillRect(120, 10, 150, 150);        // Fill a rectangle with the color
 }
+
+// Creates a vector v1 that will be red later on
+//v1 = Vector3(10, 10, 0);
+
+/**
+* Creates a vector that is draw to the screen
+* @param v input vector
+* @param color input color
+* @return nothing
+*/
+/*function drawVector(v, color) {
+lineTo((v[0] * 20, v[1] * 20), (v[2] * 20, v[3] * 20));
+}*/
+
+// drawing the red vector v1
+//drawVector(v1, "red");
