2c6c2d111352ea4b54efb515d6263d2f91cec82e
Ismail Ahmed <130091934+iyahmed@users.noreply.github.com>
2024-04-03 15:09:27 -0700

    Ninth Commit: Part 5 is done

--- asgn0.js
+++ asgn0.js
@@ -38,7 +38,7 @@ function drawVector(v, color) {
   ctx.stroke();
 }
 
-/** On the press of the draw button, clears and re-fills the black canvas and draws the v1 vector from user input
+/** On the press of the first draw button, clears and re-fills the black canvas and draws the v1 and v2 Vectors from user input
  * @params nothing
  * @return nothing
  */
@@ -47,8 +47,8 @@ function handleDrawEvent() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillRect(0, 0, 400, 400);
   // Create v1 and v2 from user input
-  let v1 = new Vector3;
-  let v2 = new Vector3;
+  let v1 = new Vector3([0,0,0]);
+  let v2 = new Vector3([0,0,0]);
   v1[0] = document.getElementById("v1_x").valueAsNumber;
   v1[1] = document.getElementById("v1_y").valueAsNumber;
   v2[0] = document.getElementById("v2_x").valueAsNumber;
@@ -57,4 +57,49 @@ function handleDrawEvent() {
   // Draw v1 and v2 into the canvas
   drawVector(v1, "red");
   drawVector(v2, "blue");
+  // To draw the green line(s) (v3 and possibly v4)
+  handleDrawOperationEvent();
+}
+
+/** On the press of the second draw button, clears and re-fills the black canvas and computer mathematical operations on the v1 and v2 Vectors from user input
+ * @params nothing
+ * @return nothing
+ */
+function handleDrawOperationEvent() {
+  // Clear and re-fill the black canvas before use
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillRect(0, 0, 400, 400);
+  // Create v1 and v2 from user input
+  let v1 = new Vector3([0,0,0]);
+  let v2 = new Vector3([0,0,0]);
+  v1[0] = document.getElementById("v1_x").valueAsNumber;
+  v1[1] = document.getElementById("v1_y").valueAsNumber;
+  v2[0] = document.getElementById("v2_x").valueAsNumber;
+  v2[1] = document.getElementById("v2_y").valueAsNumber;
+  v1[2], v2[2] = 0;
+  // Draw v1 and v2 into the canvas
+  drawVector(v1, "red");
+  drawVector(v2, "blue");
+  // Calls the correct Vector3 operation based off the values of the selector and scalar (+, -, *, /)
+  let v3 = new Vector3([0,0,0]);
+  let v4 = new Vector3([0,0,0]);
+  let selector = document.getElementById("operation_selector").value;
+  let scalar = document.getElementById("operation_scalar").value; 
+  if (selector === "Add") {
+    v3 = v1.add(v2);
+    drawVector(v3, "green");
+  } else if (selector === "Subtract") {
+    v3 = v1.sub(v2);
+    drawVector(v3, "green");
+  } else if (selector === "Multiply") {
+    v3 = v1.mul(scalar);
+    v4 = v2.mul(scalar);
+    drawVector(v3, "green");
+    drawVector(v4, "green");
+  } else if (selector === "Divide") {
+    v3 = v1.div(scalar);
+    v4 = v2.div(scalar);
+    drawVector(v3, "green");
+    drawVector(v4, "green");
+  }
 }
